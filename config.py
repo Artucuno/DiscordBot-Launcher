7944e70f28026e4b25c93f593f699b17baff8777
Articuno1234 <artbothelp@gmail.com>
2017-05-20 08:16:47 +1000

    Launcher

---
+++ config.py
@@ -0,0 +1,13 @@
+PREFIX = ">"
+
+VERSION = "1.0"
+
+TOKEN = 'MzEzMTI5NDA3NTY2OTcwODgx.C_lIAA.2B7nrGUeyuyAFi3sFZzpZp7qXKY'
+
+OWNER = "Articuno™&© | Hexicle Creator#9712"
+
+DEVS = "Articuno™&© | Hexicle Creator#9712"
+
+STAFF = "Articuno™&© | Hexicle Creator#9712"
+
+OWNERSID = "183119845900943360"
