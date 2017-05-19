52338c0633e05a6637ddbe9c07edb074428f9324
Articuno1234 <artbothelp@gmail.com>
2017-05-20 08:32:49 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-PREFIX = ">"
+PREFIX = "Bots prefix"
 
 VERSION = "1.0"
 
-TOKEN = 'MzEzMTI5NDA3NTY2OTcwODgx.C_lIAA.2B7nrGUeyuyAFi3sFZzpZp7qXKY'
+TOKEN = 'Token Here'"
 
-OWNER = "Articuno™&© | Hexicle Creator#9712"
+DEVS = "Who you are working with"
 
-DEVS = "Articuno™&© | Hexicle Creator#9712"
+STAFF = "Helpers"
 
-STAFF = "Articuno™&© | Hexicle Creator#9712"
-
-OWNERSID = "183119845900943360"
+OWNERSID = "Your id"
