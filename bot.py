d5b6b6c7ce985093b0c6f4e4b3239862b340424c
Articuno1234 <artbothelp@gmail.com>
2017-05-25 10:02:10 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,8 +147,8 @@ async def github(ctx):
     """DiscordBot-Launcher's repo"""
     em = discord.Embed(color=0x0BFCF2)
     em.add_field(name='Link', value=("[Click Here!](https://github.com/Articuno1234/DiscordBot-Launcher)"))
-    em.add_field(name='Version', value=config.VERSION))
-    em.set_thumbnail(url= "https://assets-cdn.github.com/images/spinners/octocat-spinner-128.gif")
+    em.add_field(name='Version', value=config.VERSION)
+    em.set_thumbnail(url= "https://assets-cdn.github.comem.add_field(name='Link', value=("[Click Here!]("https://github.com/Articuno1234/DiscordBot-Launcher)")))/images/spinners/octocat-spinner-128.gif")
     await bot.say(embed = em)
     
 
